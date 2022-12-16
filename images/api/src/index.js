cf616d6beb5d1377059feac21f20ab2773b4b4d7
niko.brusselaers <niko.brusselaers@student.ehb.be>
2022-12-16 23:17:46 +0100

    fix(api): added cors to api

--- images/api/src/index.js
+++ images/api/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const bodyParser = require('body-parser')
 const connection = require('./database/connection.js')
+const cors = require('cors')
 const app = express()
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(bodyParser.json())
+app.use(cors())
 app.use(express.static("../docs"))
 
 
