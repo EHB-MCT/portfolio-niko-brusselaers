12ffbdc26919442c63a8637b70fd8d5e1a988f7d
niko.brusselaers <niko.brusselaers@student.ehb.be>
2022-12-18 14:29:05 +0100

    feat(front-end): created function getAllRooms
    created function getAllRoom to receive all roomNames and insert them in navigation

---
+++ images/front-end/src/fetch/getAllRooms.js
@@ -0,0 +1,22 @@
+let url = "http://localhost:3001"
+
+/**
+ * Do a fetch call to rest api and retrieve all rooms,
+ * and insert hyperlink to roomData in navigation.
+ * 
+ * @returns
+ */
+const getAllRooms = () => {
+    fetch(`${url}/getAllRooms`)
+        .then(response => response.json())
+        .then(data => {
+            const roomLinks = document.getElementById('roomLinks')
+            let innerHTML = ``
+            data.roomNames.forEach(element => {
+                innerHTML += `<a href="#">${element}</a>`
+            });
+            roomLinks.innerHTML = innerHTML
+        })
+}
+
+export default getAllRooms
---
+++ images/front-end/src/index.js
@@ -0,0 +1,11 @@
+import getAllRooms from "./fetch/getAllRooms.js"
+import getRoomData from "./fetch/getRoomData"
+
+
+/**
+ * get all rooms when loading page and insert all roomnames in navigation
+ * 
+ * @see getAllRooms
+ */
+getAllRooms()
+
